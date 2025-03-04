9defe47dc8f5e15e87c5721512cb8b3ee7a2acd9
Amal VK <55309949+Amalvk@users.noreply.github.com>
2025-03-04 13:33:59 +0530

    Update Test2.jsx

--- src/Components/Test2.jsx
+++ src/Components/Test2.jsx
@@ -21,9 +21,9 @@ function Test2(props) {
   const displayDate = (date) =>{
     const options = {year:'numeric',month:'short',day:'numeric'}
     const today = new Date().toLocaleDateString("en-US",options)
-    const wishDate = new Date(date).toLocaleDateString("en-US"options)
+    const wishDate = new Date(date).toLocaleDateString("en-US",options)
     
-    if today == wishDate return "Today"
+    if (today == wishDate) return "Today"
     else return wishDate
   }
   return (
